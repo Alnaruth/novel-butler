2db1ce696c4b7fc1b72f3e6eab8b1ea096d53180
Alberto_Tessari <tessarialberto.vr@gmail.com>
2022-11-17 20:59:08 +0100

    Created branch for obsolete version

--- src/freewebnovel.py
+++ src/freewebnovel.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+# test for commit
 import os
 import sys
 
